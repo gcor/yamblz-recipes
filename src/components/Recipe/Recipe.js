46ec30d43bb1a22ec20b70af5dbbdba665bfb806
Anton Ahatov <ahatov@gismeteo.ru>
2016-08-19 20:58:18 +0300

    add recipeItem folder

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,24 +2,6 @@ import React, { Component } from 'react'
 import { View, Text} from 'react-native'
 
 class Recipe extends Component {
-  constructor () {
-    super()
-    this.state = {
-      data: 'no data'
-    }
-  }
-  componentWillMount () {
-    this.setState({
-      data: 'no data'
-    })
-
-    fetch('http://google.com')
-      .then(r => {
-        this.setState({
-          data: r.status
-        })
-      })
-  }
   render () {
     return (
       <Text>{this.state.data}</Text>
