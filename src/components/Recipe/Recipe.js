517be8ea8a2f68b59a673f50e5944d839188b54b
Anton Ahatov <ahatov@gismeteo.ru>
2016-08-20 16:56:35 +0300

    recipe note markup

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import RecipeItem from '../RecipeItem/RecipeItem'
+import css from './Recipe.css'
 
 class Recipe extends Component {
 	render () {
 		return (
-			<View>
+			<View style={css.recipe}>
 				{this.props.recipeItemData.map((item, index) => {
 					return <RecipeItem key={index} recipeItemData={item} />
 				})}
+				<Text style={css.recipe__note}>Мы напомним когда нужно будет проверить или помешать</Text>
 			</View>
 		)
 	}
