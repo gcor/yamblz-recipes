9e18ab004c8a8d857e0cf8fbf85e42c31e5c75d3
phil romanov <romanovexports@gmail.com>
2016-08-24 22:05:22 +0300

    Add some measures

---
+++ src/containers/List/util.js
@@ -0,0 +1,17 @@
+const measures = {
+	absolute: {
+		kg: 'кг',
+		ml: 'мл'
+	},
+	relative: {
+		spoon: ['ложка', 'ложки', 'ложек']
+	},
+	independent: {
+		egg: {
+			multi: true,
+			value: ['яйцо', 'яйца', 'яиц']
+		}
+	}
+}
+
+export default measures
