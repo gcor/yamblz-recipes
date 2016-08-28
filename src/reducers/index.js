c80200b70178489a2744000d342d20fa52581ae9
phil romanov <romanovexports@gmail.com>
2016-08-28 16:29:02 +0300

    Quick fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-import tabs from './tabs'
 import recipe from './recipe'
 import navigationState from './navigationState'
 
 const reducers = {
-	tabs,
 	recipe,
 	navigationState
 }
