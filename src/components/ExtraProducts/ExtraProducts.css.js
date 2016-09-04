24141d622094adbb32856129da668032a853fe0e
Artem Luchin <artem.luchin@gmail.com>
2016-09-04 20:14:12 +0300

    RecipeView page redesign

---
+++ src/components/ExtraProducts/ExtraProducts.css.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Dimensions } from 'react-native'
+import ui from '../../constants/css'
+
+export default StyleSheet.create({
+	extraProducts: {},
+	extraProducts__title: {
+		fontSize: ui.fontS,
+		color: 'rgba(0,0,0,.56)',
+		marginLeft: 24,
+		marginBottom: 16,
+		marginTop: 24
+	},
+	extraProducts__item: {
+		width: Dimensions.get('window').width * 0.4,
+		marginLeft: 8,
+		marginRight: 5,
+		marginBottom: 10
+	}
+})
--- src/containers/RecipeView/RecipeView.js
+++ src/containers/RecipeView/RecipeView.js
@@ -3,11 +3,16 @@ import {
 	ScrollView,
 	View,
 	Image,
+	Text,
+	ListView,
 	InteractionManager
 } from 'react-native'
 import Button from '../../components/Button'
-import RecipeTabs from '../../components/RecipeTabs'
+import IngredientList from '../../components/IngredientList'
+import ExtraProducts from '../../components/ExtraProducts'
 import css from './RecipeView.css'
+import { LOADING, SUCCESS, ERROR } from '../../constants/actionTypes'
+import Preloader from '../../components/Preloader'
 
 export default class RecipeView extends Component {
 	componentWillMount () {
@@ -26,20 +31,53 @@ export default class RecipeView extends Component {
 		this.props.resetRecipe()
 	}
 
-	renderTabs () {
-		const { isReady } = this.state
-		const imageSrc = 'http://' + this.props.recipe.image
+	renderIngredientList () {
+		const { status } = this.props.recipe
 		const { incrementRecipePortion, decrementRecipePortion } = this.props
+		const imageSrc = 'http://' + this.props.recipe.image
+		const products = [
+			{
+				title: 'Перец черный молотый',
+				image: 'http://img3.board.com.ua/a/2004653239/wm/1-perets-chernyij-dushistyij-belyij-chili.jpg'
+			},
+			{
+				title: 'Тимьян',
+				image: 'http://r-azbuka.ru/img_catalog/preview/1437149732.jpg'
+			},
+			{
+				title: 'Лавровый лист',
+				image: 'http://expertpokozhe.ru/wp-content/uploads/2016/02/Lavrovyj-list-dlja-lechenija-pryshhej.jpg'
+			}
+		]
+		switch (status) {
+			case LOADING: return (
+				<Preloader margin={80} />
+			)
+			case SUCCESS: return (
+				<View style={css.recipe}>
+					<Image source={{uri: imageSrc}} style={css.recipe__image} />
+					<IngredientList
+						tabLabel='Продукты'
+						onDecrement={decrementRecipePortion}
+						onIncrement={incrementRecipePortion}
+						recipe={this.props.recipe}
+					/>
+					<ExtraProducts
+						title={'можно добавить'}
+						id={'1'}
+						products={products} />
+				</View>
+			)
+			case ERROR: return 'Сломалось или нет Интернета'
+		}
+	}
+
+	renderContent () {
+		const { isReady } = this.state
 		if (isReady) {
 			return (
 				<ScrollView style={{backgroundColor: 'white'}}>
-					<View style={css.recipe}>
-						<Image source={{uri: imageSrc}} style={css.recipe__image} />
-						<RecipeTabs
-							onIncrement={incrementRecipePortion}
-							onDecrement={decrementRecipePortion}
-						/>
-					</View>
+					{this.renderIngredientList()}
 				</ScrollView>
 			)
 		}
@@ -49,7 +87,7 @@ export default class RecipeView extends Component {
 	render () {
 		return (
 			<View style={{flex: 1}}>
-				{this.renderTabs()}
+				{this.renderContent()}
 				<Button
 					onPress={this._onPress.bind(this)}
 					text='Приготовить' />
