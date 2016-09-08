a61b30fa8f7b6d386a744f4006ead60c5ddde487
gcor <anton@gcor.ru>
2016-09-09 01:06:04 +0300

    add custov appmetrika event

--- android/app/src/main/java/com/kitchen/MainApplication.java
+++ android/app/src/main/java/com/kitchen/MainApplication.java
@@ -22,6 +22,7 @@ import java.util.List;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.sensormanager.SensorManagerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.kitchen.appmetrika.AppMetrikaPackage;
 import com.yandex.metrica.YandexMetrica;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -38,7 +39,8 @@ public class MainApplication extends Application implements ReactApplication {
 					new MainReactPackage(),
 					new ReactNativePushNotificationPackage(),
 					new SensorManagerPackage(),
-					new LinearGradientPackage()
+					new LinearGradientPackage(),
+					new AppMetrikaPackage()
 			);
 		}
 	};
