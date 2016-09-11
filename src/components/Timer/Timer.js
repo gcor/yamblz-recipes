c1becad66c9dd42daeb1636fed97d51655e5bf27
phil romanov <romanovexports@gmail.com>
2016-09-11 19:59:45 +0300

    Add animation to the timer button

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,12 +1,12 @@
 import React, { Component, PropTypes } from 'react'
 import {
 	View, Text,
-	TouchableOpacity
+	TouchableOpacity,
+	Animated,
 } from 'react-native'
 import s from './Timer.css'
 import listCSS from '../List/List.css'
-import { formatTime } from './util'
-import Notifications from '../Notification'
+// import Notifications from '../Notification'
 
 class Timer extends Component {
 	constructor () {
@@ -19,6 +19,8 @@ class Timer extends Component {
 			textValue: 'Включить таймер'.toUpperCase(),
 			activated: false
 		})
+		this._animatedButtonColor = new Animated.Value(0)
+		this._animatedTextColor = new Animated.Value(0)
 	}
 
 	handlePress () {
@@ -34,25 +36,41 @@ class Timer extends Component {
 			textValue: 'Таймер установлен'.toUpperCase(),
 			activated: true
 		})
+		const config = {toValue: 100, duration: 500}
+		Animated.parallel([
+			Animated.timing(this._animatedButtonColor, config),
+			Animated.timing(this._animatedTextColor, config)
+		]).start()
 	}
 
 	render () {
 		const { timerLabel } = this.props
 		// console.log(actionLabel, timerLabel)
 		// let time = formatTime(timeout)
-		const { textValue, activated } = this.state
+		const { textValue } = this.state
+
+		var buttonColor = this._animatedButtonColor.interpolate({
+			inputRange: [0, 100],
+			outputRange: ['#FFEC3B', '#F1EFEC']
+		})
+
+		var textColor = this._animatedTextColor.interpolate({
+			inputRange: [0, 100],
+			outputRange: ['black', '#A3A2A0']
+		})
+
 		return (
 			<View style={s.timer}>
 				<View style={[listCSS.item, s.item]}>
 					<View style={[listCSS.item__point, s.yellowPoint]}></View>
 					<Text style={listCSS.item__value}>{timerLabel}</Text>
 				</View>
-				<TouchableOpacity style={
-					[s.button, activated ? s.buttonInactive : {}]
-					} onPress={this.handlePress}>
-					<Text style={
-						[s.buttonText, activated ? s.textInactive : {}]
-					}>{textValue}</Text>
+				<TouchableOpacity onPress={this.handlePress} activeOpacity={0.9}>
+					<Animated.View style={[s.button, {backgroundColor: buttonColor}]}>
+						<Text style={
+							[s.buttonText, {color: textColor}]
+						}>{textValue}</Text>
+					</Animated.View>
 				</TouchableOpacity>
 			</View>
 		)
