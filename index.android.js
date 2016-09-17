f0f4120d7ed495c62ddbeab80f3d7ca7db5da845
gcor <anton@gcor.ru>
2016-09-17 14:23:33 +0300

    refactor package.json

--- index.android.js
+++ index.android.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native'
-import Wrapper from './src/app'
-AppRegistry.registerComponent('kitchen', () => Wrapper)
+import App from './src/app'
+AppRegistry.registerComponent('kitchen', () => App)
