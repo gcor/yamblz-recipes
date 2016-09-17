3f54f0a31e2971bb0d4972f7eeac69d30c070836
gcor <anton@gcor.ru>
2016-09-17 14:03:46 +0300

    killl old components

--- src/components/Slider/Slider.css.js
+++ src/components/Slider/Slider.css.js
@@ -1,8 +1,7 @@
-import { StyleSheet, Dimensions } from 'react-native'
+import { StyleSheet } from 'react-native'
 import ui from '../../constants/css'
 
 export default StyleSheet.create({
-	slider: {},
 	slider__title: {
 		fontSize: ui.fontS,
 		color: 'rgba(0,0,0,.56)',
--- src/containers/RecipeView/RecipeView.css.js
+++ src/containers/RecipeView/RecipeView.css.js
@@ -4,30 +4,30 @@ export default StyleSheet.create({
 	recipe__image: {
 		height: Dimensions.get('window').height * 0.5
 	},
-    recipe__background: {
-        flex: 1,
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        backgroundColor: 'rgba(0,0,0,0.1)'
-    },
-    recipe__gradient1: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        height: 239,
-        opacity: 0.2
-    },
-    recipe__gradient2: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        height: 128
-    }
+	recipe__background: {
+		flex: 1,
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0,
+		backgroundColor: 'rgba(0,0,0,0.1)'
+	},
+	recipe__gradient1: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0,
+		height: 239,
+		opacity: 0.2
+	},
+	recipe__gradient2: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0,
+		height: 128
+	}
 })
