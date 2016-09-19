9ddd65178058c3947412bc7d98200746fd5f1b33
Artem Luchin <artem.luchin@gmail.com>
2016-09-19 19:30:23 +0300

    guess whos back

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,41 +1,48 @@
 import React, { Component, PropTypes } from 'react'
-import { Text, View, ListView, ViewPagerAndroid } from 'react-native'
+import { Text, View, ListView } from 'react-native'
 import Card from '../Card/Card'
 import css from './Slider.css'
-import { debounce } from 'lodash'
 
 export default class Slider extends Component {
+	constructor (props) {
+		super(props)
+		// create recipes list from props.recipes
+		this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+		// init state
+		this.state = {
+			dataSource: this.ds.cloneWithRows(props.recipes)
+		}
+	}
+	componentWillReceiveProps (props) {
+		// update when state change
+		this.setState({
+			dataSource: this.ds.cloneWithRows(props.recipes)
+		})
+	}
 	render () {
 		return (
 			<View style={css.slider}>
 				<Text style={css.slider__title}>{this.props.title.toUpperCase()}</Text>
-				<ViewPagerAndroid
-					style={{height: 370, width: 400}}>
-					{this._renderCards()}
-				</ViewPagerAndroid>
-			</View>
-		)
-	}
-
-	_renderCards () {
-		const { recipes } = this.props
-		if (recipes.length === 0) return null
-		return recipes.map(el => this._renderCard(el))
-	}
-	_renderCard (cardData) {
-		return (
-			<View key={cardData._id} style={{paddingLeft: 10}}>
-				<Card
-					data={cardData}
-					onPressHandler={this.props.onPressHandler}
-					style={css.slider__item}
+				<ListView
+					horizontal
+					enableEmptySections
+					showsHorizontalScrollIndicator={false}
+					dataSource={this.state.dataSource}
+					renderRow={this._renderCard.bind(this)}
 				/>
 			</View>
 		)
 	}
+	_renderCard (cardData) {
+		return (
+			<Card
+				data={cardData}
+				onPressHandler={this.props.onPressHandler}
+				style={css.slider__item}
+			/>
+		)
+	}
 }
-
-
 
 Slider.propTypes = {
 	onPressHandler: PropTypes.func.isRequired,
