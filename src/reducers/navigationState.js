c494db9fe951404c01eef784242cd7956475c9b3
phil romanov <romanovexports@gmail.com>
2016-09-20 15:45:44 +0400

    Revert "prepare for animations"

--- src/reducers/navigationState.js
+++ src/reducers/navigationState.js
@@ -7,8 +7,7 @@ import {
 const initialNavState = {
 	index: 0,
 	routes: [
-		{ key: 'Animations', title: 'Анимации' }
-		// { key: 'Home', title: 'Меню' }
+		{ key: 'Home', title: 'Меню' }
 	]
 }
 
